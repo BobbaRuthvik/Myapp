c842127cd6b67c1b210399c9d219a3eaaf90ccf6
cs19b004 Greeshmitha <cs19b004@iittp.ac.in>
2021-08-29 22:28:28 +0530

    Image upload

---
+++ app/src/main/java/com/example/myapp/uploadinfo.java
@@ -0,0 +1,25 @@
+package com.example.myapp;
+
+public class uploadinfo {
+    public String imageName;
+    public String imagePrice;
+    public String imageURL;
+    public uploadinfo(){}
+
+    public uploadinfo(String name, String url,String price) {
+        this.imageName = name;
+        this.imageURL = url;
+        this.imagePrice=price;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+    public String getImageURL() {
+        return imageURL;
+    }
+    public String getImagePrice() {
+        return imagePrice;
+    }
+}
+
